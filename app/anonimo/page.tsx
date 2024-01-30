0c7e046e9c40ba3b70671b7bffab1caa45e568ac
Rangel Koli <rangelkoli@gmail.com>
2024-01-30 13:37:23 -0500

    Added about me section in the home page

--- app/anonimo/page.tsx
+++ app/anonimo/page.tsx
@@ -14,14 +14,35 @@ import "./styles.css";
 import FindXHome from "../../assets/images/FindXHomeScreen.png";
 import laptop from "../../assets/animations/laptopX.json";
 import { Kaushan_Script } from "next/font/google";
-import AnonTitle from "../../assets/images/anonimo/AnonHome.png";
+import AnonTitle from "../../assets/images/anonimo/AnonHome.jpeg";
 import ExternalLink from "../../assets/images/ExternalLink.svg";
 import Link from "next/link";
 import FindXMain from "../../assets/images/FindXNew1.png";
 import architecture from "../../assets/images/anonimo/architectureDia.png";
 import Navbar from "../components/Navbar";
 import { Container } from "@mui/material";
-
+const features = [
+  {
+    name: 'Push to deploy',
+    description:
+      'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+  },
+  {
+    name: 'SSL certificates',
+    description:
+      'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
+  },
+  {
+    name: 'Simple queues',
+    description:
+      'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
+  },
+  {
+    name: 'Advanced security',
+    description:
+      'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
+  },
+]
 const kaushenScript = Kaushan_Script({ subsets: ["latin"],
   display: "swap",
   weight: "400"
@@ -36,19 +57,36 @@ const Anonimo = () => {
   }, []);
 
   return (
-    <div>
+    <div style={{
+
+    }}>
       <Navbar />
-      <section className="anominoProject" id="anominoProject">
+
+
+      <section className="anominoProject" id="anominoProject" style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "2rem",
+        marginBottom: "2rem",
+
+        width: "100%",
+        height: "100%",
+        paddingTop: "2rem",
+        paddingBottom: "2rem",
+      }}>
         <Container maxWidth="md">
-          <div className="projectHeader" style={{
+      <div className="projectHeader" style={{
             display: "flex",
             flexDirection: "row",
             justifyContent: "space-between",
             alignItems: "center",
             marginTop: "2rem",
+            width: "100%",
             
           }}>
-              <div className="flex flex-col justify-center" style={{ flex: 1 }}>
+              <div className="flex px-4 flex-col justify-center" style={{ flex: 1 }}>
                 
                 <Typography
                   variant="h5"
@@ -83,19 +121,91 @@ const Anonimo = () => {
               </div>
             
           </div>
-          <div className="flex flex-col items-center justify-center">
-            <div className="flex flex-col items-center justify-center">
-              <div className="flex flex-col items-center justify-center">
-                <Image
-                  className="rounded-xl"
-                  src={AnonTitle}
-                  alt="Album"
-                  style={{ marginTop: "2rem",width: "100%" }}
-                />
-              </div>
+          </Container>
 
-              <div className="flex flex-col items-center justify-center">
-              <div className="flex flex-col justify-center" style={{ flex: 1 }}>
+      <div className="headerMainForAnonimo"
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "2rem",
+      
+      }}>
+       
+      <div className="mockup-browser border border-base-300 px-4" style={{
+    width: "80%",
+      }}>
+  <div className=" mx-auto sm-12 mockup-browser-toolbar" style={{
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    
+  }}>
+ 
+    <div className="input border border-base-300">https://anonimo.com</div>
+  </div>
+  <div className="flex justify-center border-t border-base-300">
+    <Image 
+      className=""
+      src={AnonTitle}
+      alt="Album"
+      style={{width: "100%" }}
+    />
+  </div>
+  </div>
+  </div>
+  <span className="flex items-center">
+  <span className="h-px flex-1 bg-white"></span>
+  <span className="shrink-0 px-6">Lorem, ipsum dolor</span>
+  <span className="h-px flex-1 bg-white"></span>
+</span>
+        <Container maxWidth="md">
+
+        
+          <div className="flex flex-col items-center justify-center">
+              
+
+                
+              <div className="bg-black py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:text-center">
+          <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white-900 sm:text-4xl">
+            Everything you need to deploy your app
+          </p>
+          <p className="mt-6 text-lg leading-8 text-white-600">
+            Quis tellus eget adipiscing convallis sit sit eget aliquet quis. Suspendisse eget egestas a elementum
+            pulvinar et feugiat blandit at. In mi viverra elit nunc.
+          </p>
+        </div>
+        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            {features.map((feature) => (
+              <div key={feature.name} className="relative pl-16">
+                <dt className="text-base font-semibold leading-7 text-white-900">
+                  <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                  </div>
+                  {feature.name}
+                </dt>
+                <dd className="mt-2 text-base leading-7 text-white-600">{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
+              <div className="projectMainDescription" style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginTop: "2rem",
+            
+          }}>
+              <div className="flex flex-col-8 justify-center" style={{ flex: 1 }}>
                 
                 <Typography
                   variant="h5"
@@ -128,7 +238,11 @@ const Anonimo = () => {
                   </Typography>
                 </div>
               </div>
-                </div>
+            
+          </div>
+
+              <div className="flex flex-col items-center justify-center">
+           
 
               <div className="flex flex-col items-center justify-center">
                 <Image
@@ -138,6 +252,196 @@ const Anonimo = () => {
                   style={{ marginTop: "2rem", width: "100%" }}
                 />
               </div>
+              <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+   
+              <h2 className="mt-4 text-xl font-bold text-white sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-center">
+ Architectural Overview
+</h2>
+    <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+
+      <a
+        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="/services/digital-campaigns"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-10 w-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
+
+        <h2 className="mt-4 text-xl font-bold text-white">Home Page</h2>
+
+        <p className="mt-1 text-sm text-gray-300">
+        The initial landing page provides users with a concise overview of the website's purpose and functionality upon their first visit.
+        </p>
+      </a>
+
+      <a
+        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="/services/digital-campaigns"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-10 w-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
+
+        <h2 className="mt-4 text-xl font-bold text-white">Anonym Page</h2>
+
+        <p className="mt-1 text-sm text-gray-300">
+        This interactive platform allows users to engage in various activities such as posting, liking, commenting, and interacting with other community members.
+        </p>
+      </a>
+
+      <a
+        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="/services/digital-campaigns"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-10 w-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
+
+        <h2 className="mt-4 text-xl font-bold text-white">Login/Signup</h2>
+
+        <p className="mt-1 text-sm text-gray-300">
+        Serving as the website's gateway, this page requires user authentication through login. Alternatively, new users can create an account by clicking on the signup link to access all features.
+        </p>
+      </a>
+
+      <a
+        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="/services/digital-campaigns"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-10 w-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
+
+        <h2 className="mt-4 text-xl font-bold text-white">Profile</h2>
+
+        <p className="mt-1 text-sm text-gray-300">
+        The user-specific profile page displays detailed information, including the user's name, bio, and activity statistics such as the number of posts, followers, and following counts. Users can manage connections by sending friend requests or unfriending others. Additionally, the chat functionality is accessible through this page.
+        </p>
+      </a>
+
+      <a
+        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="/services/digital-campaigns"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-10 w-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
+
+        <h2 className="mt-4 text-xl font-bold text-white">Resources</h2>
+
+        <p className="mt-1 text-sm text-gray-300">
+        The Resources page curates valuable and informative content, including blogs, books, and articles sourced from various corners of the internet.
+        </p>
+      </a>
+
+      <a
+        className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+        href="/services/digital-campaigns"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-10 w-10 text-pink-500"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path d="M12 14l9-5-9-5-9 5 9 5z" />
+          <path
+            d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+          />
+        </svg>
+
+        <h2 className="mt-4 text-xl font-bold text-white">Admin Panel</h2>
+
+        <p className="mt-1 text-sm text-gray-300">
+        The default Django admin panel serves as the backend interface for Database Administrators (DBA), enabling them to access the database and perform necessary operations.
+        </p>
+      </a>
+    </div>
+
+  
+  </div>
 
               <div className="flex flex-col items-center justify-center">
                 <Typography
@@ -192,6 +496,10 @@ const Anonimo = () => {
               </div>
             </div>
           </div>
+          <section className="bg-black text-white">
+            
+  
+</section>
         </Container>
       </section>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -399,6 +399,102 @@ style={{
 
 
 <section className="aboutme" id="aboutme">
+<section className="flex items-center bg-stone-50 xl:h-screen font-poppins dark:bg-gray-800 ">
+        <div className="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
+            <div className="flex flex-wrap items-center ">
+                <div className="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+                    <div className="lg:max-w-md">
+                        <span className="text-xl font-semibold text-blue-600 uppercase dark:text-blue-500">
+                            About ME!</span>
+                        <h2 className="mt-4 mb-6 text-2xl  dark:text-gray-300" style={{
+                          textAlign: "justify"
+                        }}>
+                        Greetings! I am Rangel Anselm Koli, a passionate and dedicated software developer based in Syracuse, NY. Pursuing a Master's in Computer Science from Syracuse University and a strong foundation in Information Technology from Mumbai University, I am committed to contributing my technical acumen to drive innovation and facilitate organizational growth in the dynamic field of IT.</h2>
+         
+                    </div>
+                </div>
+                <div className="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
+                    <div className="flex mb-4">
+                        <span
+                            className="flex items-center justify-center flex-shrink-0 w-12 h-12 mr-6 bg-blue-500 rounded dark:bg-blue-500 dark:text-gray-100 text-gray-50">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                className="w-5 h-5 bi bi-file-earmark-code" viewBox="0 0 16 16">
+                                <path
+                                    d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h-2z" />
+                                <path
+                                    d="M8.646 6.646a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L10.293 9 8.646 7.354a.5.5 0 0 1 0-.708zm-1.292 0a.5.5 0 0 0-.708 0l-2 2a.5.5 0 0 0 0 .708l2 2a.5.5 0 0 0 .708-.708L5.707 9l1.647-1.646a.5.5 0 0 0 0-.708z" />
+                            </svg>
+                        </span>
+                        <div>
+                            <h2 className="mb-4 text-xl font-bold leading-tight dark:text-gray-300 md:text-2xl">
+                            Education
+                            </h2>
+                            <p className="text-base leading-loose text-gray-500 dark:text-gray-400">
+                              Masters - Computer Science, Syracuse University
+                            </p>
+                            <p className="text-base leading-loose text-gray-500 dark:text-gray-400">
+                              Bachelors - Information Technology, Mumbai University
+                            </p>
+                            
+                        </div>
+                    </div>
+                    <div className="flex mb-4">
+                        <span
+                            className="flex items-center justify-center flex-shrink-0 w-12 h-12 mr-6 bg-blue-500 rounded dark:bg-blue-500 dark:text-gray-100 text-gray-50">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                className="w-5 h-5 bi bi-file-text" viewBox="0 0 16 16">
+                                <path
+                                    d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z" />
+                                <path
+                                    d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z" />
+                            </svg>
+                        </span>
+                        <div>
+                            <h2 className="mb-4 text-xl font-bold leading-tight dark:text-gray-300 md:text-2xl">
+                            Skills
+                            </h2>
+                            <p className="text-base leading-loose text-gray-500 dark:text-gray-400">
+                            Programming Languages: C, C++, Java, Python
+                            </p>
+                            <p className="text-base leading-loose text-gray-500 dark:text-gray-400">
+                            Web Technologies: HTML, CSS, JavaScript, React, Node.js
+                            </p>
+                            <p className="text-base leading-loose text-gray-500 dark:text-gray-400">
+                            Databases: MySQL, MongoDB
+                            </p>
+                            <p className="text-base leading-loose text-gray-500 dark:text-gray-400">
+                            Tools: Git, Docker, Kubernetes
+                            </p>
+
+                        </div>
+                    </div>
+                    <div className="flex mb-4">
+                        <span
+                            className="flex items-center justify-center flex-shrink-0 w-12 h-12 mr-6 bg-blue-500 rounded dark:bg-blue-500 dark:text-gray-100 text-gray-50">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                className="w-5 h-5 bi bi-bank2" viewBox="0 0 16 16">
+                                <path
+                                    d="M8.277.084a.5.5 0 0 0-.554 0l-7.5 5A.5.5 0 0 0 .5 6h1.875v7H1.5a.5.5 0 0 0 0 1h13a.5.5 0 1 0 0-1h-.875V6H15.5a.5.5 0 0 0 .277-.916l-7.5-5zM12.375 6v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zm-2.5 0v7h-1.25V6h1.25zM8 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2zM.5 15a.5.5 0 0 0 0 1h15a.5.5 0 1 0 0-1H.5z">
+                                </path>
+                            </svg>
+                        </span>
+                        <div>
+                            <h2 className="mb-4 text-xl font-bold leading-tight dark:text-gray-300 md:text-2xl">
+                                Achievements & Certifications
+                            </h2>
+                            <p className="text-base leading-loose text-gray-500 dark:text-gray-400">
+                            1st Place in the Code Crush 1.0 Hackathon organized by St. Francis Institute of Technology
+                            </p>
+                            <p className="text-base leading-loose text-gray-500 dark:text-gray-400">
+                            Coursera - Programming Foundations with JavaScript, HTML and CSS (2020)
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
   </section>
 
 
